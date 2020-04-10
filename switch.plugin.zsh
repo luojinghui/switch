4192c87001c6efdcbc90de086ac0a199dd39fc8d
luojinghui <luojinghui@xylink.com>
2020-04-10 12:01:19 +0800

    feat: init tools

---
+++ switch.plugin.zsh
@@ -0,0 +1,6 @@
+# This plugin is MIT licensed to match the switch license.
+#
+# Make switch easy to install and keep up to date if you're using a
+# ZSH framework like Zgen or Antigen
+
+export PATH=$(dirname $0):${PATH}
